0c96b8cadcf115d11ae0447a513cd077b1a6da21
eubro <eliasjunior1405@gmail.com>
2023-07-29 23:28:30 -0300

    simple create form

--- WebApplication2/Services/SellerService.cs
+++ WebApplication2/Services/SellerService.cs
@@ -19,5 +19,12 @@ namespace WebApplication2.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+
+        }
     }
 }
